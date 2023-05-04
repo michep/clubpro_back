42548e1187f233e75131211ac6e59c7d4d9e078f
michep <>
2023-05-04 11:02:34 +0300

    search user by login

--- api/user.py
+++ api/user.py
@@ -4,10 +4,15 @@ from services.dbuser import DBUser
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
-@bp.route('/<userid>', methods=['GET'])
+@bp.route('/id/<userid>', methods=['GET'])
 @jwt_required()
-def getUser(userid: str):
+def getUserById(userid: str):
     return jsonify(DBUser.getUserById(userid))
+
+@bp.route('/login/<login>', methods=['GET'])
+@jwt_required()
+def getUserByLogin(login: str):
+    return jsonify(DBUser.getUserByLogin(login))
 
 @bp.route('', methods=['PUT'])
 @jwt_required()
