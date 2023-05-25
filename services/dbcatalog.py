cc27abcdf674ac2bd7f2fa9b2ce48ce929f352fc
michep <>
2023-05-25 22:13:11 +0300

    get catalog folders in order

--- services/dbcatalog.py
+++ services/dbcatalog.py
@@ -16,10 +16,10 @@ class DBCatalog:
         return MDB.catalog.find_one({'_id': id})
 
     @classmethod
-    def getSubFoldersByParentId(self, id: str | ObjectId | None) -> dict | None:
+    def getSubFoldersByParentId(self, id: str | ObjectId | None) -> list:
         if type(id) is str:
             id = ObjectId(id)
-        return list(MDB.catalog.find({'parentfolder_id': id}))
+        return list(MDB.catalog.find({'parentfolder_id': id}, sort = [('order', 1)]))
 
     @classmethod
     def createFolder(self, data: dict) -> dict:
@@ -34,13 +34,13 @@ class DBCatalog:
         return {'modified_count': res.modified_count, 'upserted_id': res.upserted_id}
 
     @classmethod
-    def getFolderPrimaryProducts(self, id: str | ObjectId) -> dict | None:
+    def getFolderPrimaryProducts(self, id: str | ObjectId) -> list:
         if type(id) is str:
             id = ObjectId(id)
         return list(MDB.product.find({'parentfolder_id': id}))
 
     @classmethod
-    def getFolderSecondaryProducts(self, id: str | ObjectId) -> dict | None:
+    def getFolderSecondaryProducts(self, id: str | ObjectId) -> list:
         if type(id) is str:
             id = ObjectId(id)
         return list(MDB.product.find({'secondaryfolder_ids': id}))
